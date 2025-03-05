02984a27e8a497f55096b8529860505527720df6
Pierre <galarretajeanpierre@gmail.com>
2025-03-04 23:14:16 -0500

    añadiendo configuraciones

--- index.js
+++ index.js
@@ -85,10 +85,12 @@ app.post('/api/notes', (request, response) => {
 });
 
 app.use((request, response) => {
-    response.status(404).send('<h1>404</h1>');
+    response.status(404).json({
+        error: 'Not found'
+    });
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
